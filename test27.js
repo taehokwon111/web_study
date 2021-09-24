5861621964471e62b18b7222051352f5bd10c4bc
권태호 <taehokwon48@gmail.com>
2021-09-24 13:21:33 +0900

    27

--- test27.js
+++ test27.js
@@ -1,14 +1,17 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const fs = require('fs');
+// const path = require('path');
+
+// app.get('/', (req, res) => {
+//     var file = path.join(`${__dirname}/content`);
+//     console.log(`${__dirname}/content/test.txt`)
+//     res.download(`${__dirname}/content/test.txt`);
+//     res.send('Hello World!')
+//     // res.download(file);
+// });
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
 const app = express();
-const fs = require('fs');
-const path = require('path');
-
-app.get('/', (req, res) => {
-    var file = path.join(`${__dirname}/content`);
-    console.log(`${__dirname}/content/test.txt`)
-    res.download(`${__dirname}/content/test.txt`);
-    res.send('Hello World!')
-    // res.download(file);
-});
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const express = require('express');
