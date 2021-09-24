8c589110841b1244030aa20d0e8d38e94a4cdb55
권태호 <taehokwon48@gmail.com>
2021-09-24 16:58:41 +0900

    js27_주석제거, vue~6

--- js/test27.js
+++ js/test27.js
@@ -1,17 +1,14 @@
-// const express = require('express');
-// const app = express();
-// const fs = require('fs');
-// const path = require('path');
-
-// app.get('/', (req, res) => {
-//     var file = path.join(`${__dirname}/content`);
-//     console.log(`${__dirname}/content/test.txt`)
-//     res.download(`${__dirname}/content/test.txt`);
-//     res.send('Hello World!')
-//     // res.download(file);
-// });
-
-// app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-const app = express();
 const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+app.get('/', (req, res) => {
+    var file = path.join(`${__dirname}/content`);
+    console.log(`${__dirname}/content/test.txt`)
+    res.download(`${__dirname}/content/test.txt`);
+    res.send('Hello World!')
+    // res.download(file);
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
